67a44834929fe20cefe4e282171f73886e8c9af1
Mike Partelow <github@mikepartelow.com>
2024-11-25 08:27:56 -0800

    data

--- src/tracker/mod.rs
+++ src/tracker/mod.rs
@@ -1,10 +1,15 @@
 use std::fmt;
 use std::fs::File;
-use std::io::{self, Read};
+use std::io::{self, Read, Seek, SeekFrom};
 
 // https://www.aes.id.au/modformat.html
 
-pub struct Sample {
+pub struct Module {
+    title: String,
+    samples: Vec<Sample>,
+}
+
+struct SampleHeader {
     name: [u8; 22],
     length: u16,
     finetune: u8,
@@ -13,7 +18,12 @@ pub struct Sample {
     loop_length: u16,
 }
 
-impl Sample {
+struct Sample {
+    header: SampleHeader,
+    data: [u8; 1024],
+}
+
+impl SampleHeader {
     fn from_bytes(bytes: &[u8]) -> Self {
         Self {
             name: bytes[0..22].try_into().unwrap(),
@@ -25,16 +35,12 @@ impl Sample {
         }
     }
 }
-pub struct Module {
-    title: String,
-    samples: Vec<Sample>,
-}
 
 impl fmt::Display for Module {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "title: [{}]", self.title)?;
         for (i, s) in self.samples.iter().enumerate() {
-            let name = String::from_utf8_lossy(&s.name).to_string();
+            let name = String::from_utf8_lossy(&s.header.name).to_string();
             write!(f, "\n  sample {:02}: [{}]", i, name)?;
         }
         Ok(())
@@ -56,16 +62,39 @@ fn read_title(file: &mut File) -> io::Result<String> {
     Ok(String::from_utf8_lossy(&buffer).to_string())
 }
 
-const NUM_SAMPLES: usize = 16; // there could be 31 in some versions, how to tell?
+// FIXME: there could be 15 in some versions, have to check for M.K. marker
+const NUM_SAMPLES: usize = 31;
 
 fn read_samples(file: &mut File) -> io::Result<Vec<Sample>> {
     let mut samples: Vec<Sample> = Vec::new();
 
     for _ in 1..=NUM_SAMPLES {
-        let mut buffer = vec![0; std::mem::size_of::<Sample>()];
+        let mut buffer = vec![0; std::mem::size_of::<SampleHeader>()];
         file.read_exact(&mut buffer)?;
 
-        samples.push(Sample::from_bytes(&buffer));
+        samples.push(Sample {
+            header: SampleHeader::from_bytes(&buffer),
+            data: [0; 1024],
+        });
+    }
+
+    // FIXME: actually, we need to read and parse this stuff
+    file.seek(SeekFrom::Current(1 + 1 + 128))?;
+
+    let mut buffer: Vec<u8> = vec![0; 4];
+    file.read_exact(&mut buffer)?;
+    let mk = String::from_utf8_lossy(&buffer).to_string();
+
+    // FIXME: actually, first read 15 samples, check for this stuff, then backtrack if we don't find it.
+    if mk != "M.K." {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!("Error: missing expected magic marker M.K."),
+        ));
+    }
+
+    for s in samples.iter_mut() {
+        file.read_exact(&mut s.data)?;
     }
 
     Ok(samples)
